06ada57a6dc4e02b293e895c9e44a91b439a770d
justintischendorf <154767599+justintischendorf@users.noreply.github.com>
2026-02-09 16:05:21 +0100

    feature: implemented endpoits GET & POST

--- app/REST/index.ts
+++ app/REST/index.ts
@@ -1,5 +1,33 @@
-import { Elysia } from 'elysia'
+import { Elysia } from "elysia";
+import { MessageModel } from "./model";
+import type { Message } from "../Utils";
 
-new Elysia()
-    .get('/ping', 'pong')
-    .listen(3000)
+let messages: Message[] = [];
+
+new Elysia({ prefix: "/messages" })
+
+  .post(
+    "/",
+    ({ body, set }) => {
+      body: body;
+
+      if (body.content && body.sender != undefined) {
+        set.status = 201;
+        const message: Message = body;
+        messages.push(message);
+        return message;
+      } else {
+        set.status = 400;
+        return new Error("Missing arguments.");
+      }
+    },
+    {
+      body: MessageModel.PostMessageBody,
+    },
+  )
+
+  .get("/", ({ set }) => {
+    set.status = 200;
+    return messages;
+  })
+  .listen(3000);
--- app/REST/model.ts
+++ app/REST/model.ts
@@ -1,1 +1,8 @@
-import { t } from 'elysia'
+import { t } from "elysia";
+
+export namespace MessageModel {
+  export const PostMessageBody = t.Object({
+    sender: t.String(),
+    content: t.String(),
+  });
+}
