250cf4a685cf7d67e7e7d221a9c5346f56137c36
justintischendorf <154767599+justintischendorf@users.noreply.github.com>
2026-02-10 15:28:24 +0100

    feature: connected API and databank

---
+++ apps/rest/src/message/service.ts
@@ -0,0 +1,23 @@
+import { prisma } from "../../../../packages/database/src";
+import type { Message } from "../../modules/utils";
+import type { MessageModel } from "./model";
+
+export abstract class MessageService {
+  static async createMessage({
+    body,
+  }: {
+    body: (typeof MessageModel.PostMessageBody)["static"];
+  }) {
+    const createdMessage = await prisma.message.create({
+      data: {
+        ...body,
+      },
+    });
+    return createdMessage;
+  }
+
+  static async getAllMessages() {
+    const messages = await prisma.message.findMany();
+    return messages;
+  }
+}
